225e5bd51f9b51f6ea9a0b2cb3ccd1c57f853f20
Antor Khan <mkhan143127@bscse.uiu.ac.bd>
2018-09-30 04:49:36 +0600

    Version 1.0.3

--- app/src/main/java/info/antor/www/amarnotes/MainActivity.java
+++ app/src/main/java/info/antor/www/amarnotes/MainActivity.java
@@ -9,20 +9,60 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 
 public class MainActivity extends AppCompatActivity {
 
     EditText noteText;
     DatabaseHelper databaseHelper;
+    private AdView adView;
+    String addUnitKey="ca-app-pub-7485001410940669/2514328687";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        MobileAds.initialize(this,addUnitKey);
         databaseHelper = new DatabaseHelper(this);
+        adView=findViewById(R.id.adView);
+        AdRequest adRequest=new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+//        adView.setAdListener(new AdListener() {
+//            @Override
+//            public void onAdLoaded() {
+//                // Code to be executed when an ad finishes loading.
+//                Toast.makeText(getApplicationContext(),"onAdload",Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onAdFailedToLoad(int errorCode) {
+//                // Code to be executed when an ad request fails.
+//                Toast.makeText(getApplicationContext(),"Failed to load",Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onAdOpened() {
+//                // Code to be executed when an ad opens an overlay that
+//                // covers the screen.
+//                Toast.makeText(getApplicationContext(),"add opened",Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onAdLeftApplication() {
+//                // Code to be executed when the user has left the app.
+//            }
+//
+//            @Override
+//            public void onAdClosed() {
+//                // Code to be executed when when the user is about to return
+//                // to the app after tapping on an ad.
+//            }
+//        });
+
 
     }
     public boolean insertData(View view){
@@ -32,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
 
         boolean flag=databaseHelper.insertData(input);
         if(flag){
+            noteText.clearComposingText();
             Toast.makeText(getApplicationContext(),"Note Added",Toast.LENGTH_SHORT).show();
+            noteText.getText().clear();
         }
         else{
 
@@ -73,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clearAll(View view) {
+
         boolean flag=databaseHelper.clearAll();
         if(flag)
             Toast.makeText(this,"Cleared", Toast.LENGTH_SHORT).show();
